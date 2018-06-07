56981975f0bde2d2dc2818b378d3d6aab15c423c
DESKTOP-HJ62G7L\Jet <jetamin94@gmail.com>
2018-06-08 01:36:35 +0700

    update

--- getAdress/app.js
+++ getAdress/app.js
@@ -1,5 +1,4 @@
 let list = $("td:nth-child(2) a");
-
 for (let i = 0; i <= list.length; i++) {
   console.log(list[i].text)
 }
--- getTransactions/app.js
+++ getTransactions/app.js
@@ -5,28 +5,42 @@ const fs = require('fs');
 const wallet_url = '../getAdress/wallet_demo.csv'
 
 const wallets = fs.readFileSync(wallet_url, 'utf8')
-const arrays = wallets.split('\r\n');
-console.log('start...', arrays);
+const walletsArr = wallets.split('\r\n');
+console.log('start...', walletsArr);
 
-console.log('getTokensTranfer===', getTokensTranfer);
-// const params = {
-//   // startblock: 1,
-//   // endblock: 10,
-//   action: 'tokentx',
-//   page: 1,
-//   offset: 10,
-//   sort: 'desc'
-// }
+// console.log('getTokensTranfer===', getTokensTranfer);
+const params = {
+  // startblock: 1,
+  // endblock: 10,
+  action: 'tokentx',
+  page: 1,
+  offset: 10,
+  sort: 'desc'
+}
 
-for (let adress of arrays) {
-  const url = `https://api.etherscan.io/api?module=account&action=balance&address=${adress}&tag=latest&apikey=`
-  // const url = getTokensTranfer(adress, '', params)
+let data = {};
+let txts = [];
+let content = ''
+
+for (let address of walletsArr) {
+  // const url = `https://api.etherscan.io/api?module=account&action=balance&address=${address}&tag=latest&apikey=`
+  const url = getTokensTranfer(address, '', params)
   const xhttp = new XMLHttpRequest();
+  console.log('Sending request...')
   xhttp.open("GET", url, false);
   xhttp.setRequestHeader("Content-type", "application/json");
   xhttp.send();
-  const response = xhttp.responseText;
-  console.log('response===', response);
+  txts.push(xhttp.responseText.result);
+  // console.log(JSON.parse(xhttp.responseText).result);
+  data[address] = JSON.parse(xhttp.responseText).result;
+  // content = xhttp.responseText;
 }
 
+console.log(data);
+// var dateString = moment.unix(value).format("MM/DD/YYYY");
+
+// fs.writeFile('Txts.json', content, function (err) {
+//   if (err) throw err;
+//   console.log('Saved!');
+// });
 
