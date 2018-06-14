76e438fce6305e11a1ce89ab596a10826cf1b22f
DESKTOP-HJ62G7L\Jet <jetamin94@gmail.com>
2018-06-15 01:31:48 +0700

    update

--- telegramBot/app.js
+++ telegramBot/app.js
@@ -1,6 +1,25 @@
-const Telegraf = require('telegraf')
+const Telegraf = require('telegraf');
 const api = '592709609:AAEaPvzFgijM0wWpwQaChzZs5AfhbnHi7jg';
 
 const bot = new Telegraf(api)
-bot.command('/test1', (ctx) => ctx.reply('Hello'))
-bot.startPolling()
+bot.command('/test', (ctx) => ctx.replyWithMarkdown('*Hello*'));
+bot.startPolling();
+
+
+// const request = require('request');
+// const base_url = 'https://api.telegram.org/bot'
+// const method = 'sendMessage'
+// let url = `${base_url}${api}/${method}`;
+// const params = {
+//   chat_id: '@jtool',
+//   text: 'Test'
+// }
+// request.post(
+//   url,
+//   { json: params },
+//   function (error, response, body) {
+//     if (!error && response.statusCode == 200) {
+//       console.log(body)
+//     }
+//   }
+// );
