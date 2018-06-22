24c3e15469fdb36b0875ed5d72aa22abb731f228
ThangNV <jetamin94@gmail.com>
2018-06-22 13:58:17 +0700

    update

--- getTransactions/src/bot.js
+++ getTransactions/src/bot.js
@@ -2,9 +2,10 @@ import Telegraf from 'telegraf';
 import { TELEGRAM_KEY } from './const';
 
 function test(ctx) {
-  // setTimeout(() => ctx.replyWithMarkdown('hi'), 3000);
   ctx.replyWithMarkdown('*Hii*');
 }
+
+console.log('Bot is ready now!');
 
 const bot = new Telegraf(TELEGRAM_KEY)
 bot.command('/test', test);
